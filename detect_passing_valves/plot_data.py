fbaa0e2939794245d4701a522a518a799a4ce9e7
Carlos Duarte <cduarte@berkeley.edu>
2021-11-05 13:14:28 -0700

    added legends to timeseries plots

--- detect_passing_valves/plot_data.py
+++ detect_passing_valves/plot_data.py
@@ -8,11 +8,11 @@ from os.path import join
 
 from bokeh.io import show, save, output_file
 from bokeh.layouts import column
-from bokeh.models import ColumnDataSource, RangeTool, LinearAxis, Range1d, BoxAnnotation
+from bokeh.models import ColumnDataSource, RangeTool, LinearAxis, Range1d, BoxAnnotation, Legend
 from bokeh.plotting import figure
 
 
-def parse_list_timestamps(str_ts_list, tz='UTC'):
+def parse_list_timestamps(str_ts_list, time_format="Timestamp('%Y-%m-%d %H:%M:%S%z', tz='UTC')"):
     """
     Convert a list of pandas timestamps represented as a string to a
     readable format i.e. pandas timestamp object
@@ -22,7 +22,7 @@ def parse_list_timestamps(str_ts_list, tz='UTC'):
     fault_dates = re.split(r'(?<=\)\)), ', fault_dates)
     f_dates = [re.split(r'(?<=\)), ', ts[1:-1]) for ts in fault_dates]
 
-    f_dts = [pd.to_datetime(ts, format="Timestamp('%Y-%m-%d %H:%M:%S%z', tz='{}')".format(tz)) for ts in f_dates]
+    f_dts = [pd.to_datetime(ts, format=time_format) for ts in f_dates]
 
     return f_dts
 
@@ -49,7 +49,9 @@ def plot_valve_data(csv_path, fault_dates=None, fig_folder='./'):
         right_y_min = np.floor(vlv_dat['air_flow'].min())
         sub_cols.append('air_flow')
 
-    src = ColumnDataSource(vlv_dat.loc[:, sub_cols])
+    subset_dat = vlv_dat.loc[:, sub_cols]
+    src = ColumnDataSource(subset_dat)
+    index_name = subset_dat.index.name
 
     # make the plot
     max_date_idx = min(480, len(vlv_dat.index))
@@ -70,20 +72,30 @@ def plot_valve_data(csv_path, fault_dates=None, fig_folder='./'):
             p.add_layout(box_ann)
 
     # line plots
-    p.step('index', 'upstream_ta', source=src, color='#7093db', line_width=2)
-    p.step('index', 'dnstream_ta', source=src, color='#db7093', line_width=2)
+    p.step(index_name, 'upstream_ta', source=src, color='#7093db', line_width=2, legend_label="Upstream temp")
+    p.step(index_name, 'dnstream_ta', source=src, color='#db7093', line_width=2, legend_label="Downstream temp")
 
     if 'air_flow' in vlv_dat.columns:
         p.extra_y_ranges = {"vlvPos": Range1d(start=-1, end=101),
                             "airFlow": Range1d(start=right_y_min*(1-y_overlimit), end=right_y_max*(1+y_overlimit))
                             }
         p.add_layout(LinearAxis(y_range_name='airFlow', axis_label='Air flow [cfm]'), 'right')
-        p.step('index', 'air_flow', source=src, color='#93db70', y_range_name='airFlow', line_width=2)
+        p.step(index_name, 'air_flow', source=src, color='#93db70', y_range_name='airFlow', line_width=2, legend_label="Airflow rate")
     else:
         p.extra_y_ranges = {"vlvPos": Range1d(start=-1, end=101)}
 
+    # add valve data
     p.add_layout(LinearAxis(y_range_name='vlvPos', axis_label='Valve position [%]'), 'left')
-    p.step('index', 'vlv_po', source=src, color='#9a9a9a', line_width=0.5, line_dash='4 4', y_range_name='vlvPos')
+    p.step(index_name, 'vlv_po', source=src, color='#9a9a9a', line_width=0.5, line_dash='4 4', y_range_name='vlvPos', legend_label="Valve position")
+
+    # add legend
+    p.add_layout(Legend(), 'right')
+    p.legend.click_policy = "hide"
+
+    p.legend.label_text_font_size = "9px"
+    p.legend.label_height = 5
+    p.legend.glyph_height = 5
+    p.legend.spacing = 5
 
     # range selector tool
     range_tool = RangeTool(x_range=p.x_range)
@@ -97,14 +109,23 @@ def plot_valve_data(csv_path, fault_dates=None, fig_folder='./'):
                     tools="", toolbar_location=None, background_fill_color="#ffffff")
     select.yaxis.axis_label = 'Valve'
 
-    select.step('index', 'vlv_po', source=src, color='#70dbb8')
+    select.step(index_name, 'vlv_po', source=src, color='#70dbb8', legend_label="Valve position")
     select.ygrid.grid_line_color = None
     select.add_tools(range_tool)
     select.toolbar.active_multi = range_tool
 
     select.extra_y_ranges = {"tempDiff": Range1d(start=right_sec_y_min*(1-y_overlimit), end=right_sec_y_max*(1+y_overlimit))}
-    select.add_layout(LinearAxis(y_range_name='tempDiff', axis_label='TDiff'), 'right')
-    select.step('index', 'temp_diff', source=src, color='#b870db', y_range_name='tempDiff')
+    select.add_layout(LinearAxis(y_range_name='tempDiff', axis_label='TDiff [F]'), 'right')
+    select.step(index_name, 'temp_diff', source=src, color='#b870db', y_range_name='tempDiff', legend_label="Temp Difference")
+
+    # add legend
+    select.add_layout(Legend(), 'right')
+    select.legend.click_policy = "hide"
+
+    select.legend.label_text_font_size = "9px"
+    select.legend.label_height = 5
+    select.legend.glyph_height = 5
+    select.legend.spacing = 5
 
     if fault_dates is not None:
         for box_ann in fault_hilight:
@@ -117,7 +138,7 @@ def plot_valve_data(csv_path, fault_dates=None, fig_folder='./'):
     save(column(p, select))
 
 
-def plot_fault_valves(vlv_dat_folder, fault_dat_path, fig_folder):
+def plot_fault_valves(vlv_dat_folder, fault_dat_path, fig_folder, time_format="Timestamp('%Y-%m-%d %H:%M:%S%z', tz='UTC')"):
     """
     Plot timeseries data for valves that were detected as passing valves
     """
@@ -134,7 +155,7 @@ def plot_fault_valves(vlv_dat_folder, fault_dat_path, fig_folder):
     for idx, df_row in fault_dat.iterrows():
         if pd.notnull(df_row['long_term_fail_str_end_dates']):
             fault_dates = df_row['long_term_fail_str_end_dates']
-            fault_dates = parse_list_timestamps(fault_dates)
+            fault_dates = parse_list_timestamps(fault_dates, time_format=time_format)
             vlv_name = "{}-{}-{}".format(df_row['site'], df_row['equip'], df_row['vlv'])
             csv_names = [f for f in all_csv_files if vlv_name in f]
 
@@ -159,7 +180,12 @@ def plot_good_valves(vlv_dat_folder, sample_size, fig_folder):
     good_valve_files = [tail.split('.png')[0] for tail in good_valve_files]
 
     # sample define number of files
-    sample_files = random.sample(good_valve_files, sample_size)
+    if sample_size == 'all':
+        sample_files = good_valve_files
+    elif sample_size > len(good_valve_files):
+        sample_files = good_valve_files
+    else:
+        sample_files = random.sample(good_valve_files, sample_size)
 
     for sf in sample_files:
         # plot fault data
@@ -175,17 +201,17 @@ def plot_good_valves(vlv_dat_folder, sample_size, fig_folder):
 if __name__ == '__main__':
 
     # define data sources
-    project_folder = join("./", "with_airflow_checks_year_end")
+    project_folder = join('./', 'external_analysis', 'lg_4hr_shrt_1hr_updated_no_off_period')
     vlv_dat_folder = join(project_folder, "csv_data")
 
     # fault data plots
     fig_folder_faults = join(project_folder, 'timeseries_valve_faults')
     fault_dat_path = join(project_folder, "passing_valve_results.csv")
 
-    plot_fault_valves(vlv_dat_folder, fault_dat_path, fig_folder_faults)
+    plot_fault_valves(vlv_dat_folder, fault_dat_path, fig_folder_faults, time_format="Timestamp('%Y-%m-%d %H:%M:%S')")
 
     # good data plots
     fig_folder_good = join(project_folder, 'timeseries_valve_good')
-    plot_good_valves(vlv_dat_folder, sample_size=20, fig_folder=fig_folder_good)
+    plot_good_valves(vlv_dat_folder, sample_size='all', fig_folder=fig_folder_good)
 
 
