02a414856b60696d14a0c804a49a3ffada82d167
Carlos Duarte <cduarte@berkeley.edu>
2022-04-04 08:53:41 -0700

    end of march plot updates

--- hot_water_temperature_reset/analysis/request_smap_data.py
+++ hot_water_temperature_reset/analysis/request_smap_data.py
@@ -380,7 +380,7 @@ if __name__ == "__main__":
 
     # time interval for to download data
     start = dtutil.dt2ts(dtutil.strptime_tz("1-01-2022", "%m-%d-%Y"))
-    end   = dtutil.dt2ts(dtutil.strptime_tz("03-31-2022", "%m-%d-%Y"))
+    end   = dtutil.dt2ts(dtutil.strptime_tz("04-02-2022", "%m-%d-%Y"))
 
     # initiate smap client and download tags
     smap_client = SmapClient(url, key=keyStr)
